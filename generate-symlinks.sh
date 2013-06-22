a3827dc80c5a4d110f4aa5453ac02074f6c7ac11
Sam Hewitt <hewittsamuel@gmail.com>
2013-06-21 20:00:05 -0400

    Minor restructure.

--- generate-symlinks.sh
+++ generate-symlinks.sh
@@ -30,7 +30,7 @@ cd ..
 echo 'Done.'
 
 # Generate symlinks for 22x22 icons
-echo 'Generating symlinks for 22x22 pixel icons.'
+echo 'Generating symlinks for 22x22 pixel icons...'
 cd 22x22
 icontool-map -c actions
 icontool-map -c apps
@@ -43,7 +43,7 @@ cd ..
 echo 'Done.'
 
 # Generate symlinks for 24x24 icons
-echo 'Generating symlinks for 24x24 pixel icons.'
+echo 'Generating symlinks for 24x24 pixel icons...'
 cd 24x24
 icontool-map -c actions
 icontool-map -c apps
@@ -53,6 +53,82 @@ icontool-map -c mimetypes
 icontool-map -c places
 icontool-map -c status
 cd ..
+echo 'Done.'
+
+# Generate symlinks for 32x32 icons
+echo 'Generating symlinks for 32x32 pixel icons...'
+cd 32x32
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+echo 'Done.'
+
+echo 'Will now generate symlinks missed by "icontool".'
+
+# Generate symlinks for 16x16 icons
+cd 16x16/status
+echo 'Generating links for 16x16 pixel symbolic battery icons...'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-caution.png battery-caution-symbolic.png
+ln -sf battery-caution-charging.png battery-caution-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
+echo 'Done.'
+cd ..
+cd ..
+
+# Generate symlinks for 22x22 icons
+cd 22x22/status
+echo 'Generating links for 22x22 pixel symbolic battery icons...'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-caution.png battery-caution-symbolic.png
+ln -sf battery-caution-charging.png battery-caution-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
+echo 'Done.'
+cd ..
+cd ..
+
+# Generate symlinks for 24x24 icons
+cd 24x24/status
+echo 'Generating links for 24x24 pixel symbolic battery icons...'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-caution.png battery-caution-symbolic.png
+ln -sf battery-caution-charging.png battery-caution-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
 echo 'Done.'
 
 #END
